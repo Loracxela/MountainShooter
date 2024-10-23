243c34dadddce04e896949e0aa95babc00005736
loracxela <loracxela07@gmail.com>
2024-10-23 17:09:35 -0300

    Fixing imports in Level.py file
    Signed-off-by: loracxela <loracxela07@gmail.com>

--- venv/code/Menu.py
+++ venv/code/Menu.py
@@ -1,34 +1,34 @@
-import sys
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-import pygame
+import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import COLOR_ORANGE, WIN_HEIGHT, MENU_OPTION, WIN_WIDTH, COLOR_YELLOW, COLOR_WHITE
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
         self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
-        self.surf = pygame.transform.scale(self.surf, (WIN_WIDTH, WIN_HEIGHT))  # Redimensiona a imagem
-        self.rect = self.surf.get_rect(left=0, top=0)  # Ajusta a posição da imagem
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
         menu_option = 0
-        pygame.mixer.music.load('./asset/Menu.mp3')
-        pygame.mixer.music.play(-1)
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", COLOR_ORANGE, (WIN_WIDTH // 2, WIN_HEIGHT // 5))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, (WIN_WIDTH // 2, WIN_HEIGHT // 3))
+            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, (WIN_WIDTH // 2, 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, (WIN_WIDTH // 2, 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
 
             # Check for all events
@@ -51,14 +51,7 @@ class Menu:
                         return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
-
-
-# Inicialização do Pygame
-pygame.init()
-window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-menu = Menu(window)
-menu.run()
