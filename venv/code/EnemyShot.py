845980f16a2a9edbd1fb4cf7f63d1535c9e8d4d3
Caroline Alexsandra <136416048+Loracxela@users.noreply.github.com>
2024-11-01 22:53:05 -0300

    Update EnemyShot.py

--- venv/code/EnemyShot.py
+++ venv/code/EnemyShot.py
@@ -1,27 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from abc import ABC, abstractmethod
-
-import pygame.image
-
-from code.Const import ENTITY_HEALTH, ENTITY_DAMAGE, ENTITY_SCORE
+from code.Const import ENTITY_SPEED
+from code.Entity import Entity
 
 
-class Entity(ABC):
+class EnemyShot(Entity):
+
     def __init__(self, name: str, position: tuple):
-        self.name = name
-        self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
-        self.rect = self.surf.get_rect(left=position[0], top=position[1])
-        self.speed = 0
-        self.health = ENTITY_HEALTH[self.name]
-        self.damage = ENTITY_DAMAGE[self.name]
-        self.score = ENTITY_SCORE[self.name]
-        self.last_dmg = 'None'
+        super().__init__(name, position)
 
-    @abstractmethod
-    def move(self):
-        pass
-
-
-class EnemyShot:
-    pass
+    def move(self, ):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
